353fca4fd5a115d0f123e7feedf935c64d855077
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2021-10-04 12:33:08 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,40 +13,40 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Solomon Shalom<a href="https://github.com/solomonshalom"> Lijo</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          God is Good,{' '}
+          <code className={styles.code}>Life is Good</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://github.com/solomonshalom?tab=repositories" className={styles.card}>
+            <h2>My Projects &rarr;</h2>
+            <p>All my projects, from the beginning to the lastest. Find them all.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://pastebin.com/DSEC3x2X" className={styles.card}>
+            <h2>My Social &rarr;</h2>
+            <p>All my socials on one place, also on a side note - follow and subscribe to me.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://gowatch.tk"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Yuki &rarr;</h2>
+            <p>An anime streaming website built in wordpress</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>A something about me &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              I am Solomon - Middle School student and Web/ Game Developer.
             </p>
           </a>
         </div>
